8cda9f44019babb976d7429c33779599e593cf7a
kamal <kamal.zyel@gmail.com>
2024-02-28 15:10:49 +0700

    fix navbar position

--- components/navbar/bottom.vue
+++ components/navbar/bottom.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="flex justify-between bg-white shadow-2xl p-4 rounded-t-xl">
+	<div class="flex justify-between bg-white shadow-2xl p-4 rounded-t-xl relative z-10">
 		<NavbarLink to="/" title="Beranda" :active="true">
 			<IconsHome class="w-5 h-5 mx-auto" />
 		</NavbarLink>
@@ -13,6 +13,10 @@
 			<IconsUserCircle class="w-5 h-5 mx-auto" />
 		</NavbarLink>
 	</div>
+	<!-- shadow element -->
+	<div class="absolute bg-black/20 blur-lg left-0 w-full top-0 z-0 h-10"> 
+	</div>
+	<!-- shadow element -->
 </template>
 
 <script lang="ts" setup>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,32 @@
 <template>
-	<main class="relative max-w-sm bg-slate-200 mx-auto h-screen main-wrapper overflow-hidden">
+	<main
+		class="relative w-full-center  bg-slate-200 mx-auto h-screen main-wrapper overflow-hidden flex flex-col justify-between">
 		<slot />
 		<!-- bottom navbar -->
 		<div class="absolute fixed bottom-0 w-full ">
-			<NavbarBottom/>
+			<NavbarBottom />
 		</div>
 	</main>
 </template>
 
 <script setup>
 const activemenu = ref(true)
+
+const setFullHeight = () => {
+	let vh = window.innerHeight * 0.01;
+	document.body.style.setProperty("--vh", `${vh}px`);
+}
+
+onMounted(() => {
+	setFullHeight();
+	window.addEventListener("resize", setFullHeight);
+});
+
+onUnmounted(() => {
+	window.removeEventListener("resize", setFullHeight);
+});
+
+
 </script>
 
 <style>
@@ -18,5 +35,9 @@ const activemenu = ref(true)
 	/* Use vh as a fallback for browsers that do not support Custom Properties */
 	height: calc(var(--vh, 1vh) * 100);
 	margin: 0 auto;
+}
+
+.w-full-center {
+	@apply w-full max-w-sm;
 }
 </style>
