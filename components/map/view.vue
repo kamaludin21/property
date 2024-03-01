5619ecccb188286e82e0e1d1f92a12a64c8a34a6
kamal <kamal.zyel@gmail.com>
2024-03-01 10:49:40 +0700

    props

--- components/map/view.vue
+++ components/map/view.vue
@@ -1,14 +1,14 @@
 <template>
-	<div class="w-full h-full ">
-		<client-only>
-			<LMap ref="map" zoom="16" :center="[0.5086178514786303, 101.44782858051124]" LMarker>
+	<client-only>
+		<div class="w-full h-full">
+			<LMap ref="map" zoom="16" :center="[0.5086178514786303, 101.44782858051124]">
 				<LTileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 					attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a>" layer-type="base"
 					name="OpenStreetMap" />
 				<l-marker :lat-lng="[0.5086178514786303, 101.44782858051124]"></l-marker>
 			</LMap>
-		</client-only>
-	</div>
+		</div>
+	</client-only>
 </template>
 
 <script setup>
