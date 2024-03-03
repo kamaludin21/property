0dd91fdd64a43896071f4e9eceab9726a7f16c52
kamal <kamal.zyel@gmail.com>
2024-03-03 11:31:01 +0700

    update font-size

--- pages/property-detail.vue
+++ pages/property-detail.vue
@@ -6,7 +6,7 @@ const show = ref(false)
 	<div class="h-full bg-white border overflow-auto h-screen space-y-2 scrollbar-hide">
 		<!-- image -->
 		<div class="h-auto w-full relative">
-			<PropertyImages/>
+			<PropertyImages />
 		</div>
 		<!-- Info -->
 		<div class="p-2 space-y-2">
@@ -15,7 +15,8 @@ const show = ref(false)
 				<div class="flex-1 flex items-center gap-1 text-slate-600">
 					<IconsLocation class="h-5 w-5 mt-0.5 stroke-1.7 text-amber-600" />
 					<div class="">
-						<p class="text-sm font-light text-slate-600 line-clamp-1">Payung Sekaki, Jalan Sukajadi Perum Segar Sari</p>
+						<p class="text-sm font-light text-slate-600 line-clamp-1">Payung Sekaki, Jalan Sukajadi Perum Segar Sari
+						</p>
 					</div>
 				</div>
 			</div>
@@ -25,11 +26,11 @@ const show = ref(false)
 				<p class="text-4xl line-clamp-1 font-bold text-amber-600">790.000.000,00</p>
 			</div>
 			<div class="border p-2 flex flex-row  items-center justify-between rounded-lg">
-				<div class="flex gap-2">
-					<img src="https://source.unsplash.com/W5lWvWaHpvs" class="w-12 object-cover rounded-full h-12" alt="">
-					<div class="flex-1">
-						<p class="text-bas	e text-slate-600">Jhon F. Kennedy</p>
-						<p class="text-sm text-slate-500">
+				<div class="flex items-center gap-2">
+					<img src="~/assets/images/user.jpg" class="w-12 border object-cover rounded-full h-12" alt="">
+					<div class="flex-1 -space-y-1">
+						<p class="text-lg font-medium text-slate-600">Jhon F. Kennedy</p>
+						<p class="text-sm  text-slate-500">
 							Makmur Property
 						</p>
 					</div>
@@ -44,65 +45,41 @@ const show = ref(false)
 
 			<div class="w-full h-full grid gap-4">
 				<div class="z-0 space-y-2 pb-6 w-full h-60">
-					<p class="text-slate-600 text-sm font-medium">Lokasi</p>
-					<!-- <ClientOnly fallback="Loading maps..."> -->
-					<MapView class="w-full h-full" />
-					<!-- </ClientOnly> -->
+					<p class="text-slate-700 text-xl font-medium">Lokasi</p>
+					<ClientOnly fallback="Loading maps...">
+						<MapView class="w-full h-full" />
+					</ClientOnly>
 				</div>
 
 				<div class="z-0 space-y-2">
-					<p class="text-slate-600 text-sm font-medium">Informasi Unit</p>
+					<p class="text-slate-700 text-xl font-medium">Informasi Unit</p>
+					<hr>
 					<div class="grid grid-cols-2 gap-4">
 						<div class="flex items-center gap-2">
-							<div class="p-0.5 rounded bg-amber-100">
-								<IconsHome class="h-5 w-5 text-amber-600" />
+							<div class="p-1 rounded bg-amber-100">
+								<IconsHomeMark class="h-6 w-6 text-amber-600" />
 							</div>
-							<p class="text-sm font-normal text-slate-600">Rumah</p>
+							<p class="text-lg font-normal text-slate-600">4 Kamar</p>
 						</div>
 						<div class="flex items-center gap-2">
-							<div class="p-0.5 rounded bg-amber-100">
-
-								<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-amber-600" viewBox="0 0 24 24"
-									stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-									<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-									<path d="M16 15h5v7l-2.5 -1.5l-2.5 1.5z" />
-									<path d="M20 11l-8 -8l-9 9h2v7a2 2 0 0 0 2 2h5" />
-									<path d="M9 21v-6a2 2 0 0 1 2 -2h1.5" />
-								</svg>
+							<div class="p-1 rounded bg-amber-100">
+								<IconsBath class="h-6 w-6 text-amber-600" />
 							</div>
-							<p class="text-sm font-normal text-slate-600">4 Kamar</p>
+							<p class="text-lg font-normal text-slate-600">2 Kamar Mandi</p>
 						</div>
 						<div class="flex items-center gap-2">
-							<div class="p-0.5 rounded bg-amber-100">
-
-								<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-amber-600" viewBox="0 0 24 24"
-									stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-									<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-									<path d="M4 12h16a1 1 0 0 1 1 1v3a4 4 0 0 1 -4 4h-10a4 4 0 0 1 -4 -4v-3a1 1 0 0 1 1 -1z" />
-									<path d="M6 12v-7a2 2 0 0 1 2 -2h3v2.25" />
-									<path d="M4 21l1 -1.5" />
-									<path d="M20 21l-1 -1.5" />
-								</svg>
+							<div class="p-1 rounded bg-amber-100">
+								<IconsToolsKitchen class="h-6 w-6 text-amber-600" />
 							</div>
-							<p class="text-sm font-normal text-slate-600">2 Kamar Mandi</p>
-						</div>
-						<div class="flex items-center gap-2">
-							<div class="p-0.5 rounded bg-amber-100">
-								<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-amber-600" viewBox="0 0 24 24"
-									stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-									<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-									<path
-										d="M19 3v12h-5c-.023 -3.681 .184 -7.406 5 -12zm0 12v6h-1v-3m-10 -14v17m-3 -17v3a3 3 0 1 0 6 0v-3" />
-								</svg>
-							</div>
-							<p class="text-sm font-normal text-slate-600">1 Dapur</p>
+							<p class="text-lg font-normal text-slate-600">1 Dapur</p>
 						</div>
 					</div>
 				</div>
 
 				<div class="z-0 space-y-2">
-					<p class="text-slate-600 text-sm font-medium">Deskripsi</p>
-					<p class="text-slate-700">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium mollitia
+					<p class="text-slate-700 text-xl font-medium">Deskripsi</p>
+					<hr>
+					<p class="text-slate-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium mollitia
 						atque
 						deserunt sit illum in sequi harum, nostrum recusandae dolor quae libero nemo a minus ipsam iusto vitae
 						placeat maiores?</p>
