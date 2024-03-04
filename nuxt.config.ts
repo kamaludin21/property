a9962f1c7e318950d96ac6f3534c1c2610026e6a
kamal <kamal.zyel@gmail.com>
2024-03-04 12:08:33 +0700

    add google font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-	modules: [
+  css: ['@/assets/css/main.css'],
+  modules: [
     '@nuxtjs/tailwindcss',
-		'nuxt3-leaflet',
-		'nuxt-swiper'
+    'nuxt3-leaflet',
+    'nuxt-swiper',
+    '@nuxtjs/google-fonts'
   ],
-	
+  googleFonts: {
+    display: 'swap',
+    families: {
+      Inter: true,
+      Roboto: true,
+      Raleway: true,
+      'Madimi+One': true,
+      'Playfair+Display': true
+    }
+  },
+  tailwindcss: {
+    cssPath: '~/assets/css/main.css',
+    config: {
+      theme: {
+        extend: {
+          fontFamily: {
+            inter: ['Inter'],
+            roboto: ['Roboto'],
+            raleway: ['Raleway'],
+          }
+        }
+      }
+    }
+  }
 })
