e544f0a7a08ffd29c85d2665461342e454cfd270
kamal <kamal.zyel@gmail.com>
2024-03-04 12:09:28 +0700

    fix map over container

--- pages/property-detail.vue
+++ pages/property-detail.vue
@@ -3,34 +3,41 @@ const show = ref(false)
 </script>
 
 <template>
-	<div class="h-full bg-white border overflow-auto h-screen space-y-2 scrollbar-hide">
+	<div class="h-full bg-white border overflow-auto scrollbar-hide relative">
+		<div class="absolute top-2 left-2 ">
+			<button class="p-1 bg-white/50 sticky top-5 hover:bg-white/90 rounded-lg relative z-10">
+				<IconsChevronLeft class="w-6 h-6 text-amber-600 stroke-2" />
+			</button>
+		</div>
+
 		<!-- image -->
 		<div class="h-auto w-full relative">
 			<PropertyImages />
 		</div>
 		<!-- Info -->
-		<div class="p-2 space-y-2">
-			<div>
-				<p class="text-xl font-semibold text-slate-700">Rumah Type 32 - 4 Kamar 2 Kamar Mandi</p>
-				<div class="flex-1 flex items-center gap-1 text-slate-600">
-					<IconsLocation class="h-5 w-5 mt-0.5 stroke-1.7 text-amber-600" />
+		<div class="space-y-4 p-2 font-inter">
+			<div class="space-y-1">
+				<p class="text-xl font-semibold text-slate-700 ">Rumah Type 32 - 4 Kamar 2 Kamar Mandi</p>
+				<div class="flex-1 flex items-center gap-1">
+					<IconsLocation class="h-5 w-5 stroke-1.7 text-amber-600" />
 					<div class="">
-						<p class="text-sm font-light text-slate-600 line-clamp-1">Payung Sekaki, Jalan Sukajadi Perum Segar Sari
+						<p class="text-sm font-light text-slate-600 line-clamp-1">Payung Sekaki, Pekanbaru
 						</p>
 					</div>
 				</div>
 			</div>
 			<hr>
-			<div class="flex items-top py-2 justify-center">
+			<div class="flex items-top py-2 justify-center font-roboto">
 				<p class="text-xl font-light text-slate-500">Rp.</p>
-				<p class="text-4xl line-clamp-1 font-bold text-amber-600">790.000.000,00</p>
+				<p class="text-4xl line-clamp-1 font-black text-amber-600">
+					790.000.000,00</p>
 			</div>
 			<div class="border p-2 flex flex-row  items-center justify-between rounded-lg">
 				<div class="flex items-center gap-2">
 					<img src="~/assets/images/user.jpg" class="w-12 border object-cover rounded-full h-12" alt="">
 					<div class="flex-1 -space-y-1">
-						<p class="text-lg font-medium text-slate-600">Jhon F. Kennedy</p>
-						<p class="text-sm  text-slate-500">
+						<p class="text-lg font-semibold text-slate-600 ">Jhon F. Kennedy</p>
+						<p class="text-sm text-slate-500">
 							Makmur Property
 						</p>
 					</div>
@@ -44,45 +51,51 @@ const show = ref(false)
 			</div>
 
 			<div class="w-full h-full grid gap-4">
-				<div class="z-0 space-y-2 pb-6 w-full h-60">
-					<p class="text-slate-700 text-xl font-medium">Lokasi</p>
-					<ClientOnly fallback="Loading maps...">
+				<div class="z-0 space-y-1 w-full h-auto">
+					<p class="text-lg font-medium font-semibold text-slate-700">Lokasi</p>
+					<hr>
+					<div class="w-full h-60 bg-red-200">
+							<ClientOnly fallback="Loading maps...">
 						<MapView class="w-full h-full" />
 					</ClientOnly>
+					</div>
+				
 				</div>
 
-				<div class="z-0 space-y-2">
-					<p class="text-slate-700 text-xl font-medium">Informasi Unit</p>
+				<div class="z-0 space-y-1">
+					<p class="text-lg font-medium font-semibold text-slate-700">Informasi Unit</p>
 					<hr>
-					<div class="grid grid-cols-2 gap-4">
+					<div class="grid grid-cols-2 text-slate-500 gap-4 text-base font-roboto">
 						<div class="flex items-center gap-2">
 							<div class="p-1 rounded bg-amber-100">
-								<IconsHomeMark class="h-6 w-6 text-amber-600" />
+								<IconsBed class="h-6 w-6 text-amber-600" />
 							</div>
-							<p class="text-lg font-normal text-slate-600">4 Kamar</p>
+							<p class="font-normal text-slate-600">4 Kamar</p>
 						</div>
 						<div class="flex items-center gap-2">
 							<div class="p-1 rounded bg-amber-100">
 								<IconsBath class="h-6 w-6 text-amber-600" />
 							</div>
-							<p class="text-lg font-normal text-slate-600">2 Kamar Mandi</p>
+							<p class="font-normal text-slate-600">2 Kamar Mandi</p>
 						</div>
 						<div class="flex items-center gap-2">
 							<div class="p-1 rounded bg-amber-100">
 								<IconsToolsKitchen class="h-6 w-6 text-amber-600" />
 							</div>
-							<p class="text-lg font-normal text-slate-600">1 Dapur</p>
+							<p class="font-normal text-slate-600">1 Dapur</p>
 						</div>
 					</div>
 				</div>
 
-				<div class="z-0 space-y-2">
-					<p class="text-slate-700 text-xl font-medium">Deskripsi</p>
+				<div class="z-0 space-y-1">
+					<p class=" text-lg font-medium font-semibold text-slate-700">Deskripsi</p>
 					<hr>
-					<p class="text-slate-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium mollitia
-						atque
-						deserunt sit illum in sequi harum, nostrum recusandae dolor quae libero nemo a minus ipsam iusto vitae
-						placeat maiores?</p>
+					<div class="text-base font-roboto text-slate-500">
+						<p class="text-slate-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium mollitia
+							atque
+							deserunt sit illum in sequi harum, nostrum recusandae dolor quae libero nemo a minus ipsam iusto vitae
+							placeat maiores?</p>
+					</div>
 				</div>
 
 			</div>
