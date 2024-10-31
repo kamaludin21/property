a7ba38315725e72bb431a2a8bcd65d847555d7d0
Kamaludin <kamal.zyel@gmail.com>
2024-10-31 21:32:57 +0700

    update pengajuan

--- pages/bookmark.vue
+++ pages/bookmark.vue
@@ -3,6 +3,7 @@ import { useNavbarStore } from '@/stores/navbar'
 
 const navbar = useNavbarStore()
 navbar.include()
+const activeTab = ref(0);
 const router = useRouter()
 
 const rusunawa = [
@@ -49,24 +50,36 @@ const rusunawa = [
 
 		<!-- Result card -->
 		<div class="h-screen space-y-2 overflow-y-auto scrollbar-hide p-2 bg-white rounded-lg ring-1 ring-slate-200">
-			<NuxtLink class="flex gap-x-2 p-2 bg-white shadow rounded-lg" v-for="(property, index) in rusunawa" :key="index"
-				:to="`/property-detail/${property.id}`">
-				<img :src="`/images/${property.image_path}/${index + 1}.png`" class="w-1/3 h-28 object-cover rounded-md" alt="">
-				<div class="w-2/3 h-28 flex flex-col">
-					<p class="text-base text-slate-700 font-medium">{{ property.title }}</p>
-					<div class="inline-flex items-center gap-x-1 text-slate-500 text-xs">
-						<IconsLocation class="h-3 w-3" />
-						<p>Jalan Nangka, No. 24A Gg. Johari</p>
-					</div>
-					<div class="flex-1"></div>
-					<p class="text-base font-bold text-amber-600">Rp. {{ property.start_price }} - Rp. {{ property.end_price }}
-					</p>
-				</div>
-			</NuxtLink>
+			<div class="flex bg-slate-200 border border-gray-200 rounded-lg p-1">
+				<button @click="activeTab = 0" :class="[
+					'flex-1 p-2 rounded-md',
+					activeTab === 0
+						? 'bg-white border text-amber-600'
+						: 'text-slate-400 hover:text-slate-600 hover:underline'
+				]">
+					Alur
+				</button>
+				<button @click="activeTab = 1" :class="[
+					'flex-1 p-2 rounded-md',
+					activeTab === 1
+						? 'bg-white border text-amber-600'
+						: 'text-slate-400 hover:text-slate-600 hover:underline'
+				]">
+					Persyaratan
+				</button>
+			</div>
 
-			<!-- Scroll helper -->
-			<div class="pb-44 pt-4">
-				<p class="text-sm text-center text-slate-400">End of list...</p>
+			<div class="">
+				<div v-if="activeTab == 0">
+					<img src="/images/alur.jpg" class="w-full h-auto" alt="">
+					<div class="h-44">
+					</div>
+				</div>
+				<div v-if="activeTab == 1">
+					<img src="/images/syarat.jpg" class="w-full h-auto" alt="">
+					<div class="h-44">
+					</div>
+				</div>
 			</div>
 		</div>
 
